a0de2610b445c3da696714d4987cf62b8fa9929a
AslanFSA <79552169+AslanFSA@users.noreply.github.com>
2021-03-03 19:09:27 -0300

    Create aula 2.c

---
+++ aula 2.c
@@ -0,0 +1,15 @@
+# inclui  < stdio.h >
+# inclui  < stdlib.h >
+
+/ * executa este programa usando o console pauser ou adiciona seu próprio getch, system ("pause") ou loop de entrada * /
+
+int  main ( int argc, char * argv []) {
+	int i = 0 ;
+	int j = 0 ;
+	printf ( " Digite um valor na tela: \ n " );
+	scanf ( " % i " , & i);
+	printf ( " Digite o 2 valor na tela: \ n " );
+	scanf ( " % i " , & j);
+	printf ( " Resultado: % i " , i * j);
+	// sistema ("pausa");
+}
