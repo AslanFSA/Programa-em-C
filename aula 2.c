8a67ab00c8069758d0c0e76b133170ca3f5d1d33
AslanFSA <79552169+AslanFSA@users.noreply.github.com>
2021-03-03 19:23:07 -0300

    Update aula 2.c

--- aula 2.c
+++ aula 2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 int main(int argc, char *argv[]) {
-	int i=0, j=0;
+	int i=0; 
+	int j=0;
 	printf("Digite o primeiro valor na tela:\n");
 	scanf("%d",&i);
 	printf("Digite o segundo valor na tela:\n");
