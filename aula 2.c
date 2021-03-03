793a175bae1246e16e6c2dd7401942c9401ccb6b
AslanFSA <79552169+AslanFSA@users.noreply.github.com>
2021-03-03 19:16:58 -0300

    Update aula 2.c

--- aula 2.c
+++ aula 2.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	int i=0, j=0;
 	printf("Digite o primeiro valor na tela:\n");
@@ -11,6 +9,5 @@ int main(int argc, char *argv[]) {
 	scanf("%d",&j);
 	printf("Resultado: %d \n",i+j);
 	
-	// pausar o codigo
 	system ("pause");
 }
